200126aa088c87e13d0bcef2d1388147bf94b018
vorxsaken <jhonyevans6326@gmail.com>
2024-01-11 19:01:11 +0700

    feat: route access control based on user role

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 function excludeSignInRoute(pathname: string) {
     return !pathname.startsWith('/api/user/read/signIn');
 }
+
+function excludeSiswaRoleRoute(pathname: string) {
+    if(/\/tagihan/gi.test(pathname)){
+        return /(insert|delete)/gi.test(pathname)
+    } else if(/\/user/gi.test(pathname)) {
+        return /(insert|delete|update)/.test(pathname)
+    }
+}
+
 export async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
     const { token } = await req.json();
@@ -11,16 +20,21 @@ export async function middleware(req: NextRequest) {
     if (excludeSignInRoute(pathname)) {
         if (token) {
             const secret = new TextEncoder().encode(process.env.SECRET);
-
+  
             try {
-                await jwtVerify(token, secret);
+                const { payload } = await jwtVerify(token, secret);
+                console.log(payload)
+                if(payload.role === 'SISWA' && excludeSiswaRoleRoute(pathname)) {
+                    return NextResponse.json({ message: 'unauthorized' }, { status: 401 })
+                }
+
                 return NextResponse.next()
             } catch (error) {
                 return NextResponse.json({ message: 'unauthenticated' }, { status: 401 });
             }
         }
 
-        return NextResponse.json({message: 'unauthenticated: no token'}, {status: 401})
+        return NextResponse.json({ message: 'unauthenticated: no token' }, { status: 401 })
     }
 
     return NextResponse.next()
