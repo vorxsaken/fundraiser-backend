a83cb38a234376466e3950546b6c02a733a50a9c
dinulislami <55618712+dinulislami@users.noreply.github.com>
2024-05-25 19:34:20 +0700

    Update route.ts

--- app/api/tagihan/update/route.ts
+++ app/api/tagihan/update/route.ts
@@ -26,6 +26,7 @@ export async function POST(req: Request) {
         return NextResponse.json(createTagihan, { status: 200 });
 
     } catch (error) {
+        console.log(error)
         return NextResponse.json(error, { status: 500 })
     }
 }
